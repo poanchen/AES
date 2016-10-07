5ebce162e7d1a2bac57eb6216bd5b507e046d471
Deon Liang <deonyuhaoliang@gmail.com>
2016-10-06 19:31:11 -0700

    read

--- AES.java
+++ AES.java
@@ -1,3 +1,11 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class AES extends Crypto {
 
 	static final int MODE = 0;
@@ -10,15 +18,37 @@ public class AES extends Crypto {
 	public void test() {
 		System.out.println("Call to test");
 	}
+	public static void read(BufferedReader a){
+		try{
+			String  rl = null;
+			while ((rl = a.readLine()) != null) {
+            System.out.println(rl);
+			}  
+		 }
+		catch (IOException e) {
+			e.printStackTrace();
+		}	
+	}
 	
 	public static void main(String[] args) throws Exception {
 		AES aes_265 = new AES();
-
+		File key = new File(args[KEY]);
+		File input = new File(args[PLAINTEXT]);
+		BufferedReader keyReader = new BufferedReader(new FileReader(key));
+		BufferedReader inputReader = new BufferedReader(new FileReader(input));
 		System.out.println("Hello World!");
 		aes_265.test();
-		System.out.println(args[MODE]);
-		System.out.println(args[KEY]);
-		System.out.println(args[PLAINTEXT]);
+		
+		System.out.println("THE KEY IS\n");
+		read(keyReader);
+		System.out.println("THE INPUT IS\n");
+		read(inputReader);
+		keyReader.close();
+		inputReader.close();
+		
+		//System.out.println(args[MODE]);
+		//System.out.println(args[KEY]);
+		//System.out.println(args[PLAINTEXT]);
 		// open file in java
 		// http://stackoverflow.com/questions/3806062/how-to-open-a-txt-file-and-read-numbers-in-java
 	}
