81fa0be11c0726a3f45da81e58def5f3066ee51e
poanchen <chen.baron@hotmail.com>
2016-10-10 15:37:21 -0700

    type

--- AES.java
+++ AES.java
@@ -273,7 +273,7 @@ public class AES extends Crypto {
 				}else {
 					typeOfFile = "key file";
 				}
-				System.out.println("Please make sure all the charcters in the " + typeOfFile + " is all valid charcter.");
+				System.out.println("Please make sure all the charcters in the " + typeOfFile + " is all valid character.");
 				System.out.println("Valid charcter includes '0', '1', '2', '3', '4', '5', '6', '7,', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'");
 				exitTheProgram();
 			}
